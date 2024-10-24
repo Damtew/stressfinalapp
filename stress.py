a8558fc73153e538c0632cd08c83b0a64c6ad8bf
Damtew <zolemanelu@gmail.com>
2024-10-24 14:45:44 +0300

    Update stress.py

--- stress.py
+++ stress.py
@@ -1,6 +1,7 @@
 import numpy as np
 import streamlit as st
 import pandas as pd
+import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.neighbors import KNeighborsClassifier
