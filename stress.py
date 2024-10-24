c9643943a82153071006ea389932336e661d1980
Damtew <zolemanelu@gmail.com>
2024-10-24 13:05:04 +0300

    Add files via upload

--- stress.py
+++ stress.py
@@ -1,7 +1,6 @@
 import numpy as np
 import streamlit as st
 import pandas as pd
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.neighbors import KNeighborsClassifier
