3b78f99592dc2ff0e1fb48f12a67689767cc215c
Damtew <zolemanelu@gmail.com>
2024-10-28 10:21:10 +0300

    Add files via upload

--- stress.py
+++ stress.py
@@ -1,7 +1,6 @@
 
 import streamlit as st
 import pandas as pd
-import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.neighbors import KNeighborsClassifier
